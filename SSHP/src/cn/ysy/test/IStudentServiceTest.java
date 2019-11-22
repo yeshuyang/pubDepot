b200ba61a7dd97cf61fde479401609ac0c949a14
ysy <56627214+yeshuyang@users.noreply.github.com>
2019-11-22 17:22:25 +0800

    Update IStudentServiceTest.java

--- SSHP/src/cn/ysy/test/IStudentServiceTest.java
+++ SSHP/src/cn/ysy/test/IStudentServiceTest.java
@@ -25,7 +25,7 @@ public class IStudentServiceTest {
 	public void testInsert() {
 		IStudentService service=ctx.getBean("studentServiceImpl",IStudentService.class);
 		Student s=new Student();
-		s.setName("���");
+		s.setName("112");
 		s.setAge(10);
 		try{
 			TestCase.assertEquals(service.insert(s), true);
@@ -39,7 +39,7 @@ public class IStudentServiceTest {
 		IStudentService service=ctx.getBean("studentServiceImpl",IStudentService.class);
 		Student s=new Student();
 		s.setId(11158);
-		s.setName("ÀöÀö ");
+		s.setName("Ã€Ã¶Ã€Ã¶ ");
 		s.setAge(10);
 		try{
 			TestCase.assertEquals(service.update(s), true);
